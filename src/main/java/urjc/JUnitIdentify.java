adc0ecc22f0790b3b33a77d7dcd850168981d0f2
Pablo Rivero <p.riverop@alumnos.urjc.es>
2017-10-24 14:03:54 +0200

    Identify JUNIT5 class and UT

--- src/main/java/urjc/JUnitIdentify.java
+++ src/main/java/urjc/JUnitIdentify.java
@@ -1,8 +1,36 @@
 package urjc;
 
+import java.io.File;
+
 public class JUnitIdentify {
 
-    // Get project directory
-    // Fetch pom.xml
-    // Fetch junit-jupiter
+    private File pom_directory;
+    private final String POM_FILE = "/pom.xml";
+    private final String JUNIT5_ARTEFACTID = "junit-jupiter-engine";
+
+    public JUnitIdentify(String project_path){
+        pom_directory = new File(project_path + POM_FILE);
+    }
+
+    public File getPom() {
+        return pom_directory;
+    }
+
+    public String getJunit5() {
+        return JUNIT5_ARTEFACTID;
+    }
+
+    public Boolean checkPom() {
+        return getPom().exists() && !getPom().isDirectory();
+    }
+
+
+    public Boolean checkJunit5() {
+        XPathParser x = new XPathParser();
+        x.readAndFind(getPom(), "");
+        return false;
+    }
+
+
+
 }
