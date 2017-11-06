7bf80e2c4e305fd4dbe1be7e4cbfe54d748c9b43
Pablo Rivero <p.riverop@alumnos.urjc.es>
2017-11-06 21:40:55 +0100

    Renamed Identity o Identify

---
+++ src/test/java/urjc/JavaIdentifyTest.java
@@ -0,0 +1,40 @@
+package urjc;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.File;
+
+public class JavaIdentifyTest {
+
+    private final String POM_FILE = "/pom.xml";
+    private static final String CURRENT_PROJECT_ABS_PATH = "/Users/nexmaniosis/testinglibs";
+
+    private static JavaIdentify identifyObject;
+
+    @BeforeAll
+    static void init(){
+        identifyObject = new JavaIdentify(CURRENT_PROJECT_ABS_PATH);
+    }
+
+    @Test
+    void getPom() {
+        assertEquals(new File(CURRENT_PROJECT_ABS_PATH+POM_FILE), identifyObject.getPom(), "Get pom.xml");
+    }
+
+    @Test
+    void getJunit5() {
+        assertEquals("junit-jupiter-engine", identifyObject.getJunit5(), "Find junit-jupiter-engine");
+    }
+
+    @Test
+    void checkPom() {
+        assertEquals(true, identifyObject.checkPom(), "CheckPom returns true");
+    }
+
+    @Test
+    void checkJunit5() {
+        assertEquals(true, identifyObject.checkJunit5(), "CheckJunit5 returns true");
+    }
+}
