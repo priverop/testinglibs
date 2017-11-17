bcc0dc577d65db410db41887653f50027dcdf9a7
Pablo Rivero <p.riverop@alumnos.urjc.es>
2017-11-17 22:35:27 +0100

    Removed unused File

--- src/main/java/listing/MochaListing.java
+++ src/main/java/listing/MochaListing.java
@@ -1,13 +1,10 @@
 package listing;
 
-import java.io.File;
 
 public class MochaListing extends AbstractListing{
 
-    private static final String TESTS_FOLDER = "/test/"; // Mocha tests directory by default
+    private static final String TESTS_FOLDER = "/test/";
     private static final String TESTS_EXTENSION = "js";
-
-    private File testsDirectory;
 
     public MochaListing(String project_path){
         super(project_path, TESTS_FOLDER, TESTS_EXTENSION);
