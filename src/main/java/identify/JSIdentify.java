e74c3ec21e5f8a4764e121a30a399bfaed00a4c5
Pablo Rivero <p.riverop@alumnos.urjc.es>
2017-11-17 22:35:47 +0100

    Added Jasmine & Tape identify

--- src/main/java/identify/JSIdentify.java
+++ src/main/java/identify/JSIdentify.java
@@ -6,8 +6,10 @@ import utilities.JSonManager;
 public class JSIdentify extends AbstractIdentify{
 
     private static final String JSON_FILE = "/package.json";
-    private final String MOCHA_JSON_KEY = "name";
+    private final String JSON_KEY = "name";
     private static final String MOCHA_ARTEFACTID = "mocha";
+    private static final String JASMINE_ARTEFACTID = "jasmine-core";
+    private static final String TAPE_ARTEFACTID = "tape";
 
     public JSIdentify(String project_path){
         super(project_path, JSON_FILE);
@@ -19,9 +21,23 @@ public class JSIdentify extends AbstractIdentify{
 
     public Boolean checkMocha() {
         JSonManager x = new JSonManager();
-        String y = x.readAndFind(getKeyFile(), MOCHA_JSON_KEY);
+        String y = x.readAndFind(getKeyFile(), JSON_KEY);
 
         return y.equals(MOCHA_ARTEFACTID);
+    }
+
+    public Boolean checkJasmine() {
+        JSonManager x = new JSonManager();
+        String y = x.readAndFind(getKeyFile(), JSON_KEY);
+
+        return y.equals(JASMINE_ARTEFACTID);
+    }
+
+    public Boolean checkTape() {
+        JSonManager x = new JSonManager();
+        String y = x.readAndFind(getKeyFile(), JSON_KEY);
+
+        return y.equals(TAPE_ARTEFACTID);
     }
 
 }
