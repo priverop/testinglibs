ab14ea1df433feed698dbfb3d41d4e6d915241e3
Pablo Rivero <p.riverop@alumnos.urjc.es>
2018-05-23 11:44:00 +0200

    Added Jasmine & Tape to the JUnit5ListingTest

--- src/test/java/unit/JUnit5ListingTest.java
+++ src/test/java/unit/JUnit5ListingTest.java
@@ -25,6 +25,8 @@ public class JUnit5ListingTest {
     private final String TESTNAME2 = "TEST-unit.JUnit5ListingTest";
     private final String TESTNAME3 = "TEST-unit.JSIdentifyTest";
     private final String TESTNAME4 = "TEST-unit.JavaIdentifyTest";
+    private final String TESTNAME5 = "TEST-unit.JasmineListingTest";
+    private final String TESTNAME6 = "TEST-unit.TapeListingTest";
 
     private static JUnit5Listing listingObject;
 
@@ -41,10 +43,12 @@ public class JUnit5ListingTest {
     @Test
     void getList() {
         List testList = new ArrayList<String>();
+        testList.add(TESTNAME5);
         testList.add(TESTNAME4);
         testList.add(TESTNAME3);
         testList.add(TESTNAME2);
         testList.add(TESTNAME1);
+        testList.add(TESTNAME6);
         assertEquals(testList, listingObject.getReportsName_filter(), "Get test list: 4 tests (XML).");
     }
 
